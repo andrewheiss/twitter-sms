cc01b6ca59bdc60be36a4b39860c77c459eb2d88
Andrew Heiss <andrewheiss@gmail.com>
2010-08-18 16:57:19 -0600

    Cleaned up code, added twitterlibphp submodule

--- index.php
+++ index.php
@@ -1,53 +1,60 @@
 <?php
-	phpinfo();
-	exit;
-	$config_dir = $_SERVER['DOCUMENT_ROOT'].'/../protected';
-	require("twitterlibphp/twitter.lib.php");
-	require($config_dir."/sms_config.php");
-
-	// Database configuration
-	// $db = new PDO('sqlite:'.dirname(__FILE__).'/db/db.db');
+	// Configuration
+	//-----------------------
+	// $config_dir = Directory, preferrably not accessible from the server root, with the configuration file and database files (copied from sms_config.default.php and sms.default.db)
+	$config_dir = dirname(__FILE__).'/../../protected';
 	$db = new PDO('sqlite:'.$config_dir.'/sms.db');
+	require($config_dir."/sms_config.php");
+	
+	// Load jdp's twitterlibphp (http://github.com/jdp/twitterlibphp)
+	// This should be included as a submodule in this project's git repo
+	require("lib/twitterlibphp/twitter.lib.php");
+	$twitter = new Twitter($username, $password);
+
+	// Check for most recently stored mention
 	$query = $db->query("SELECT * FROM id_tracker ORDER BY last_id DESC LIMIT 1");
 	$result = $query->fetch(PDO::FETCH_ASSOC);
 	
+	// If this is the first time, get the most recent mention and use that as the base.
+	// Obviously no e-mails will get sent during the first run.
 	if ($result) {
 		$last_id = $result['last_id'];
 	} else {
-		$last_id = 19412551379;
+		$get_last = $twitter->getMentions(array('count'=>1));
+		$last = new SimpleXMLElement($get_last);
+		$last_id = (int)$last->status->id;
 	}
-
-	$twitter = new Twitter($username, $password);
+	
+	// Check Twitter for any mentions since the last stored one
 	$check = $twitter->getMentions(array('since_id'=>$last_id));
 	$mentions = new SimpleXMLElement($check);
 
 	if ($mentions->status) {
-		// Reverse the array. This is messy. Ugh.
+		// Reverse the array.
 		foreach ($mentions->status as $status){
 			$a[] = $status;
 		}
 		$a = array_reverse($a);
 
-		// Output the most recent mentions in reverse order and e-mail them to myself
+		// Output the most recent mentions in reverse order and e-mail them to the fancy SMS e-mail address
 		foreach ($a as $status) {
 			$message = $status->user->name . " (" . $status->user->screen_name . "): " . $status->text;
 			$subject = "Tweet from " . $status->user->name;
 			$headers = "From: no-reply@andrewheiss.com";
 			
-			// wordwrap() needed?
-			
-			mail($email, $subject, $message, $headers);
+			// FUTURE: wordwrap() needed for better line breaking on the phone screen?
+			echo $message . "<br />";
+			//mail($email, $subject, $message, $headers);
 			
 			$last_id = $status->id;
 		}
 		
-		// Save the last id to the database
+		// Save the id of the most recent mention to the database
 		$query = $db->prepare("INSERT INTO id_tracker (last_id) VALUES (:last_id)");
 		$query->bindParam(':last_id', $last_id);
 		$query->execute();
 	} else {
 		// Nothing new…
-		// echo "No new mentions";
+		echo "No new mentions";
 	}
-
 ?>
